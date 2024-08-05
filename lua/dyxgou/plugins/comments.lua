6f9a99cc0ac32b4494dd9da14b828427aa407b03
dyxgou <diego5614yt@gmail.com>
2024-08-05 16:18:52 -0500

    Comments Config

---
+++ lua/dyxgou/plugins/comments.lua
@@ -0,0 +1,16 @@
+return {
+	"numToStr/Comment.nvim",
+
+	opts = {
+		toggler = {
+			---Line-comment toggle keymap
+			line = "<leader>/",
+			---Block-comment toggle keymap
+			block = "gbc",
+		},
+	},
+
+	config = function(_, opts)
+		require("Comment").setup(opts)
+	end,
+}
