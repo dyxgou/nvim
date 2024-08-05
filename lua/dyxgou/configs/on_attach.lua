a38e064dabbff4d3b563e726dd92534d135967ae
dyxgou <diego5614yt@gmail.com>
2024-08-05 16:18:29 -0500

    Lsp and stuff

---
+++ lua/dyxgou/configs/on_attach.lua
@@ -0,0 +1,32 @@
+local M = {}
+
+local map = vim.keymap.set
+
+local on_attach = function(client, bufnr)
+	local function opts(desc)
+		return { buffer = bufnr, desc = "LSP " .. desc }
+	end
+
+	map("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
+	map("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
+	map("n", "gi", vim.lsp.buf.implementation, opts("Go to implementation"))
+	map("n", "<leader>sh", vim.lsp.buf.signature_help, opts("Show signature help"))
+	map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts("Add workspace folder"))
+	map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts("Remove workspace folder"))
+	map("n", "<leader>r", vim.lsp.buf.rename, opts("Rename variable"))
+	map("n", "<leader>fe", vim.diagnostic.open_float, opts("Open float errors"))
+	map("n", "K", vim.lsp.buf.hover, opts("Hover the buffer"))
+
+	map("n", "<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, opts("List workspace folders"))
+
+	map("n", "<leader>D", vim.lsp.buf.type_definition, opts("Go to type definition"))
+
+	map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts("Code action"))
+	map("n", "gr", vim.lsp.buf.references, opts("Show references"))
+end
+
+M.on_attach = on_attach
+
+return M
