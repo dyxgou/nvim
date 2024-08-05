7141a84db215c8a0cf51fedf3f0445d8180cd949
dyxgou <diego5614yt@gmail.com>
2024-08-05 16:20:13 -0500

    Nvim Tree Config

---
+++ lua/dyxgou/configs/nvimtree.lua
@@ -0,0 +1,35 @@
+require("nvim-tree").setup({
+	renderer = {
+		root_folder_label = false,
+		icons = {
+			glyphs = {
+				default = "󰈚",
+				symlink = "",
+				folder = {
+					default = "",
+					empty = "",
+					empty_open = "",
+					open = "󰷏",
+					symlink = "",
+					symlink_open = "",
+					arrow_open = "󰁃 ",
+					arrow_closed = "󱞩 ",
+				},
+				git = {
+					unstaged = "✗",
+					staged = "✓",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "★",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
+	},
+
+	filters = {
+		git_ignored = false,
+		dotfiles = false,
+	},
+})
