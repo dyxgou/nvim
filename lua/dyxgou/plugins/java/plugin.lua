e8f6448f2c92bc8c7430fe45e24cf61cfdef1366
dyxgou <diego5614yt@gmail.com>
2024-08-09 20:12:37 -0500

    Java Config

---
+++ lua/dyxgou/plugins/java/plugin.lua
@@ -0,0 +1,15 @@
+return {
+	"nvim-java/nvim-java",
+	dependencies = {
+		"nvim-java/lua-async-await",
+		"nvim-java/nvim-java-core",
+		"nvim-java/nvim-java-test",
+		"nvim-java/nvim-java-dap",
+		"MunifTanjim/nui.nvim",
+		"neovim/nvim-lspconfig",
+		"mfussenegger/nvim-dap",
+	},
+	config = function()
+		require("java").setup()
+	end,
+}
