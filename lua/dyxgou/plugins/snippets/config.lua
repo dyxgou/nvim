d8f461f89005376684101dfea97c4f8f5cd84629
dyxgou <diego5614yt@gmail.com>
2025-01-13 22:16:45 -0500

    feat : New Snippets

---
+++ lua/dyxgou/plugins/snippets/config.lua
@@ -0,0 +1,13 @@
+local ls = require("luasnip")
+
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+ls.add_snippets("go", {
+	s("ee", {
+		t("if err != nil {"),
+		t("return err"),
+		t("}"),
+	}),
+})
