619c1df27cd40562e4b59dff7332894070cc93e0
dyxgou <diego5614yt@gmail.com>
2025-04-09 21:21:21 -0500

    add(cmp): Added cmp plugijn

---
+++ lua/dyxgou/plugins/cmp/plugin.lua
@@ -0,0 +1,16 @@
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"saadparwaiz1/cmp_luasnip",
+	},
+
+	config = function()
+		require("dyxgou.plugins.cmp.config")
+	end,
+}
