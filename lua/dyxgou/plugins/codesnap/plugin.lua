e0c481b78d1a41ff5884912d003767200591eb4b
dyxgou <diego5614yt@gmail.com>
2025-07-14 20:55:51 -0500

    add(codesnap): Added codesnap

---
+++ lua/dyxgou/plugins/codesnap/plugin.lua
@@ -0,0 +1,19 @@
+return {
+  "mistricky/codesnap.nvim",
+  build = "make",
+  keys = {
+    { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "v", desc = "Save selected code snapshot into clipboard" },
+  },
+
+  lazy = false,
+
+  config = function()
+    require("codesnap").setup({
+      title = "@dyxgou",
+      save_path = "~/Pictures",
+      watermark = "",
+      has_breadcrumbs = true,
+      has_line_number = true,
+    })
+  end,
+}
