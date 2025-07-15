3f75b534bcf0cdda6b4fbfcadf32cf3f528b0cfd
dyxgou <diego5614yt@gmail.com>
2025-07-14 20:53:40 -0500

    refactor(obsidian): Refactoring obsidian config

--- lua/dyxgou/plugins/obsidian/plugin.lua
+++ lua/dyxgou/plugins/obsidian/plugin.lua
@@ -1,37 +1,32 @@
 return {
-	"epwalsh/obsidian.nvim",
-	version = "*", -- recommended, use latest release instead of latest commit
-	ft = "markdown",
-	keys = {
-		["<cr>"] = {
-			action = function() end,
-			opts = { buffer = true, expr = true },
-		},
-	},
-	event = {
-		-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-		-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-		-- refer to `:h file-pattern` for more examples
-		"BufReadPre " .. vim.fn.expand("~", "/projects/notes/*.md"),
-		"BufNewFile " .. vim.fn.expand("~", "/projects/notes/*.md"),
-		-- "BufReadPre path/to/my-vault/*.md",
-		-- "BufNewFile path/to/my-vault/*.md",
-	},
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	opts = {
-		ui = {
-			enable = false,
-		},
-		workspaces = {
-			{
-				name = "personal",
-				path = "~/projects/notes",
-			},
-		},
-		templates = {
-			folder = "templates",
-		},
-	},
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    -- refer to `:h file-pattern` for more examples
+    "BufReadPre /home/dyxgou/projects/notes/*.md",
+    "BufNewFile /home/dyxgou/projects/notes/*.md",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/projects/notes/",
+      },
+    },
+
+    templates = {
+      folder = "99 - Meta/templates"
+    }
+  },
+
+  completion = {
+    nvim_cmp = true,
+    blink = false,
+    min_chars = 2,
+  },
 }
