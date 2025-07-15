dde97e284d756ed3b724e8935f5d25519be520d9
dyxgou <diego5614yt@gmail.com>
2025-07-14 20:53:19 -0500

    add(snippets): Added new snippets

--- lua/dyxgou/plugins/snippets/config.lua
+++ lua/dyxgou/plugins/snippets/config.lua
@@ -7,7 +7,30 @@ local i = ls.insert_node
 ls.add_snippets("go", {
   s("ee", {
     t("if err != nil {"),
-    t("return err"),
-    t("}"),
+    t({ "", "\t" }),
+    i(1, "return err"),
+    t({ "", "}" }),
   }),
+
+  s("print", {
+    t("fmt.Println("),
+    i(1),
+    t(")"),
+  }),
+
+  s("str", {
+    t("type "),
+    i(1),
+    t(" struct {"),
+    i({ "\t", 2 }),
+    t({ "", "}" })
+  })
+})
+
+ls.add_snippets("typescript", {
+  s("log", {
+    t("console.log("),
+    i(1),
+    t(");"),
+  })
 })
