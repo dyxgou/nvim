148feb8797d3ddd25f16056201dcd8246d77af3f
dyxgou <diego5614yt@gmail.com>
2025-07-14 20:53:10 -0500

    add(sql): added sql client for nvim

---
+++ lua/dyxgou/plugins/sql/plugin.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod',                     lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_disable_info_notifications = 1
+    end,
+  },
+  -- { -- optional saghen/blink.cmp completion source
+  --   'saghen/blink.cmp',
+  --   build = 'cargo build --release',
+  --   opts = {
+  --     sources = {
+  --       default = { "lsp", "path", "snippets", "buffer" },
+  --       per_filetype = {
+  --         sql = { 'snippets', 'dadbod', 'buffer' },
+  --       },
+  --       -- add vim-dadbod-completion to your completion providers
+  --       providers = {
+  --         dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+  --       },
+  --     },
+  --   },
+  -- }
+}
